2987e638dcae0d99b7c8f1a840201442c0e08738
cvood <cvood@163.com>
2022-11-28 22:44:32 +0800

    2022/11/28 fix login oauth2 config second time

--- routes/api/login.ts
+++ routes/api/login.ts
@@ -1,17 +1,14 @@
 import { HandlerContext } from "$fresh/server.ts";
-import { oauth2_client } from "../../utils/oauth2.ts"
 
 export const handler = (_req: Request, _ctx: HandlerContext): Response => {
   const url = new URL("https://accounts.google.com");
   url.pathname = 'o/oauth2/auth'
   const searchParams = {
-    access_type:"offline",
-    response_type:"code",
-    client_id:"942326127555-op7digt9fohohs8nd2j42a6gem1b9gah.apps.googleusercontent.com",
-    redirect_uri:"https://cvood-gmail-notifity.deno.dev/api/callback",
-    scope:"https://mail.google.com/",
-    state:"CWJBjJIJajgVB4C41iGuOWIPvqOrx0",
-    include_granted_scopes:"true",
+    client_id: "942326127555-op7digt9fohohs8nd2j42a6gem1b9gah.apps.googleusercontent.com",
+    redirect_uri: "https://cvood-gmail-notifity.deno.dev/api/callback",
+    response_type: "code",
+    scope: "https://mail.google.com",
+    access_type: "offline",
   };
   
   for (const k in searchParams) {
