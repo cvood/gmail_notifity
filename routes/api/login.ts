56884242f009c7171a6621e34a1773164964e88c
cvood <cvood@163.com>
2022-11-28 21:57:39 +0800

    2022/11/28 fix callback url

--- routes/api/login.ts
+++ routes/api/login.ts
@@ -8,8 +8,8 @@ export const handler = (_req: Request, _ctx: HandlerContext): Response => {
     access_type:"offline",
     response_type:"code",
     client_id:"942326127555-op7digt9fohohs8nd2j42a6gem1b9gah.apps.googleusercontent.com",
-    redirect_uri:"https%3A%2F%2Fflat-toad-34.deno.dev%2Fcallback",
-    scope:"https%3A%2F%2Fmail.google.com%2F",
+    redirect_uri:"https://cvood-gmail-notifity.deno.dev/api/callback",
+    scope:"https://mail.google.com/",
     state:"CWJBjJIJajgVB4C41iGuOWIPvqOrx0",
     include_granted_scopes:"true",
   };
