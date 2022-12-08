74dc74f8c3ca2e613e427d0bbf84a853860afe10
cvood <cvood@163.com>
2022-12-08 16:32:23 +0800

    complete fc2processer

--- routes/api/dealmessage.ts
+++ routes/api/dealmessage.ts
@@ -1,7 +1,76 @@
+
 import { HandlerContext } from "$fresh/server.ts";
 
+interface MessagePart  {
+  "partId": string,
+  "mimeType": string,
+  "filename"?: string,
+  "headers": Header[],
+  "body": MessagePartBody,
+  "parts"?: MessagePart[],
+}
+
+interface Header {
+  name: string,
+  value: string
+}
+
+interface MessagePartBody {
+  "attachmentId": string,
+  "size": number,
+  "data": string
+}
+
+interface Strategy {
+  field: string,
+  regexp: string,
+  url: string
+}
+
+function get_header(payload: MessagePart, headername:string) {
+  for (const header of payload.headers) {
+    if(header.name === headername){
+      return header.value
+    }
+  }
+  return ""
+}
+
 export const handler = async (req: Request, _ctx: HandlerContext): Promise<Response> => {
-  const message = await req.json();
-  console.log(message)
+  const payload: MessagePart = await req.json().then((message) => message.payload);
+  const strategies_resp = await fetch("http://postgrest.domcloud.io/strategies", {
+    headers: {Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoicG9zdGdyZXN0In0.6iN8feEXAPqIV_MnJDeJi-X0begyR_PtgXkB8Ddk-Z0"}
+  });
+  const strategies: Strategy[] = await strategies_resp.json();
+  for (const st of strategies) {
+    const re = new RegExp(st.regexp);
+    switch (st.field) {
+      case "From":{
+        if (!re.test(get_header(payload, "From"))) {
+          return new Response('don\'t found header "From"', {status: 500})
+        }
+        const resp = await fetch("http://localhost:8000" + st.url, {
+          method: "POST",
+          body: payload.body.data,
+        })
+        return resp
+      }
+
+      case "Subject": {
+        if (!re.test(get_header(payload, "Subject"))) {
+          return new Response('don\'t found header "Subject"', {status: 500})
+        }
+        const resp = await fetch("http://localhost:8000" + st.url, {
+          method: "POST",
+          body: payload.body.data,
+        })
+        return resp
+      }
+
+      default:
+        return new Response("no strategy match")
+    }
+  }
+
   return new Response("ok", {status: 200, headers: {'content-type': 'text/plain'}});
 }
---
+++ routes/api/fc2processer.ts
@@ -0,0 +1,26 @@
+import { HandlerContext } from "$fresh/server.ts";
+import { decode } from "https://deno.land/std/encoding/base64.ts";
+
+export const handler = async (req: Request, _ctx: HandlerContext) => {
+  const data = await req.text().then((base64) => new TextDecoder().decode(decode(base64)));
+  const re = new RegExp("https?://live.fc2.com/(\\d+)");
+  const check_health_resp = await fetch("http://yt.zhixiangtangping.top/check_health")
+  if (check_health_resp.status === 200) {
+    await fetch("http://ntfy.sh/service_status_check", {
+      method:"POST",
+      body:"yt-dlp service crashed !"
+    })
+    return new Response("video download services crashed")
+  }
+  if (re.test(data)) {
+    const live_url = re.exec(data)![0];
+    const resp = await fetch("http://yt.zhixiangtangping.top:5555/create", {
+      method: "POST",
+      body: JSON.stringify({
+        url: live_url
+      })
+    });
+    return resp
+  }
+  return new Response("this is not a live starting notifity")
+}
